e7e902a0817b4889e66ab497a2ac8efb69211b71
Tassilo <tassilo.vermeulen@hva.nl>
2021-12-03 15:17:08 +0100

    JS fix

--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,4 @@
-function travelAdvice_ValueSwap(){
+function travelAdvice_ValueSwap() {
     var tmp = document.getElementById("tA_vertrekstation").value;
     document.getElementById("tA_vertrekstation").value = document.getElementById("tA_aankomststation").value;
     document.getElementById("tA_aankomststation").value = tmp;
