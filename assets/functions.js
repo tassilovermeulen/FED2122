a4e453f702b69ffa1667a544f0dabb794bf23a64
Tassilo <tassilo.vermeulen@hva.nl>
2021-12-03 15:15:33 +0100

    UD

--- assets/functions.js
+++ assets/functions.js
@@ -1,1 +1,5 @@
-test
+function travelAdvice_ValueSwap(){
+    var tmp = document.getElementById("tA_vertrekstation").value;
+    document.getElementById("tA_vertrekstation").value = document.getElementById("tA_aankomststation").value;
+    document.getElementById("tA_aankomststation").value = tmp;
+    }
